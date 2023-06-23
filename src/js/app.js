403de4b24389a5c7e09be6fc64f8930d89fda245
Andrey Sery <andrey.72006@gmail.com>
2023-06-23 19:50:44 +0300

    Breakpoint app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,15 @@
-let dropDownBtn = document.querySelector(".select-box__select");
+const dropDownBtn = document.querySelector(".select-box__select");
 const dropDownList = document.querySelector(".select-box__list");
 const dropDownListItem = document.querySelectorAll(".select-box__item");
 
-dropDownBtn.addEventListener("click", (e) => {
-	// e.preventDefault;
+dropDownBtn.addEventListener("click", function (e) {
+	e.preventDefault;
 	dropDownList.classList.toggle("show");
 });
 
-
-
-dropDownListItem.forEach((item) => {
-	item.addEventListener("click", () => {
-    dropDownBtn.innerText = this.innerText;
-  });
+dropDownListItem.forEach(function (item) {
+	item.addEventListener("click", function () {
+		dropDownBtn.innerText = this.innerText;
+	});
 });
 
-console.log(dropDownBtn.innerText);
