544895966a924dafb2e9a96f204bee2a5e60774f
Andrey Sery <andrey.72006@gmail.com>
2023-06-24 01:01:07 +0300

    Completed "select-box"

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,36 @@
-const dropDownBtn = document.querySelector(".select-box__select");
-const dropDownList = document.querySelector(".select-box__list");
-const dropDownListItem = document.querySelectorAll(".select-box__item");
+document.querySelectorAll(".select-box").forEach(function (dropDownWrapper) {
+	const dropDownBtn = dropDownWrapper.querySelector(".select-box__select");
+	const dropDownList = dropDownWrapper.querySelector(".select-box__list");
+	const dropDownListItem =
+		dropDownWrapper.querySelectorAll(".select-box__item");
 
-dropDownBtn.addEventListener("click", function (e) {
-	e.preventDefault;
-	dropDownList.classList.toggle("show");
-});
+	// Click on the button. Open/Close select
+	dropDownBtn.addEventListener("click", function (event) {
+		event.preventDefault;
+		dropDownList.classList.toggle("show");
+	});
 
-dropDownListItem.forEach(function (item) {
-	item.addEventListener("click", function () {
-		dropDownBtn.value = this.innerText;
+	// Selecting a list item. Remember the selected value. Close the dropdown
+	dropDownListItem.forEach(function (item) {
+		item.addEventListener("click", function (event) {
+			event.stopPropagation();
+			dropDownBtn.value = this.innerText;
+			dropDownBtn.focus();
+			dropDownList.classList.remove("show");
+		});
+	});
+
+	// Click outside the dropdown. Close the dropdown
+	document.addEventListener("click", function (event) {
+		if (event.target !== dropDownBtn) {
+			dropDownList.classList.remove("show");
+		}
+	});
+
+	// Pressing Tab or Escape. Close the dropdown
+	document.addEventListener("keydown", function (event) {
+		if (event.key === "Tab" || event.key === "Escape") {
+			dropDownList.classList.remove("show");
+		}
 	});
 });
-
