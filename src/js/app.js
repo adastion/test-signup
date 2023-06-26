0d333885df0021034f5418e1293df7920d6ea0b6
Andrey Sery <andrey.72006@gmail.com>
2023-06-27 02:58:36 +0300

    Hiding list inputs and button

--- src/js/app.js
+++ src/js/app.js
@@ -41,24 +41,42 @@ document.querySelectorAll(".select-box").forEach(function (dropDownWrapper) {
 	});
 });
 
-//Work with form,
+//Work with form.
 const form = document.querySelector("form");
 const inputs = document.querySelectorAll("input");
 const submitBtn = document.querySelector(".form__btn");
 const userPassword = document.querySelector("[name='password']");
 const userConfirmPassword = document.querySelector("[name='confirm-password']");
 const userEmail = document.querySelector("[name='email']");
+const contentMain = document.querySelector(".content");
+const wrapperInputs = document.querySelector(".form__list");
+const wrapperTitle = document.querySelector('.content__block-title');
+const messageTitle = document.querySelector('.content__title');
+const messageText = document.querySelector('.content__text');
 const message = {
 	loading: "loading...",
 	success: "Ok )",
 	failure: "Disconnection",
 };
 
+//Success message. Hiding list inputs and button.
+const successMessage = () => {
+	contentMain.classList.remove('content--background-image');
+	wrapperInputs.classList.add('hidden');
+	submitBtn.classList.add('hidden');
+	form.classList.add('form--bottom')
+	wrapperTitle.classList.add('content__block-title--center')
+	messageTitle.textContent = 'Thank You!';
+	messageText.textContent = 'you registered!';
+}
+successMessage()
+
 const clearInputs = () => {
 	inputs.forEach((item) => {
 		item.value = "";
 	});
 };
+
 
 const postData = async (url, data) => {
 	document.querySelector(".show-status").textContent = message.loading;
@@ -70,38 +88,58 @@ const postData = async (url, data) => {
 	return await result.text();
 };
 
+
+// userEmail.addEventListener("input", function () {
+// 	const EMAIL_REGEXP =
+// 		/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+// 	if (!EMAIL_REGEXP.test(userEmail.value)) {
+// 		userEmail.classList.add("form__input--not-validated");
+// 	} else {
+// 		userEmail.classList.remove("form__input--not-validated");
+// 		document.querySelectorAll(".form__item").forEach((item) => {
+// 			if (item.lastElementChild === userEmail) {
+// 				item.classList.add("form__item--correct");
+// 			}
+// 			item.lastElementChild.classList.remove("form__item--correct");
+// 			item.lastElementChild.blur();
+// 		});
+// 	}
+// });
+
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
-
-	if (userPassword.value.length < 8 || userConfirmPassword.value.length < 8) {
+	if (
+		userPassword.value.length < 8 ||
+		userConfirmPassword.value.length < 8 ||
+		userPassword.value !== userConfirmPassword.value || userEmail.value === ""
+	) {
 		submitBtn.classList.remove("error");
 		submitBtn.offsetWidth;
 		submitBtn.classList.add("error");
 		userPassword.classList.add("form__input--not-validated");
 		userConfirmPassword.classList.add("form__input--not-validated");
-		console.log(item.value);
 	}
 	userPassword.classList.remove("form__input--not-validated");
 	userConfirmPassword.classList.remove("form__input--not-validated");
 
-	
 	let statusMessage = document.createElement("div");
 	statusMessage.classList.add("show-status");
 	statusMessage.style =
 		"color: red; font-weight: 500; text-align: center; padding: 4px;";
 	form.appendChild(statusMessage);
 
-	const formData = new FormData(form);
-	postData("server.php", formData)
-		.then((result) => {
-			console.log(result);
-			statusMessage.textContent = message.success;
-		})
-		.catch(() => (statusMessage.textContent = message.failure))
-		.finally(() => {
-			clearInputs();
-			setTimeout(() => {
-				statusMessage.remove();
-			}, 2000);
-		});
+	// const formData = new FormData(form);
+	// postData("server.php", formData)
+	// 	.then((result) => {
+	// 		console.log(result);
+	// 		statusMessage.textContent = message.success;
+	// 	})
+	// 	.catch(() => (statusMessage.textContent = message.failure))
+	// 	.finally(() => {
+	// 		clearInputs();
+	// 		setTimeout(() => {
+	// 			statusMessage.remove();
+	// 		}, 2000);
+	// 	});
 });
